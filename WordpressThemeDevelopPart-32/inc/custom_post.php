d4d69d5b042edb523e733c5e2036785d9add4c0c
Syed Zaman Mostafiz <zamanwebdev@gmail.com>
2023-01-17 16:50:51 +0600

    Part 33 Completed.

--- WordpressThemeDevelopPart-32/inc/custom_post.php
+++ WordpressThemeDevelopPart-32/inc/custom_post.php
@@ -84,6 +84,7 @@ function custom_service(){
       'hierarchial' => true,
       'show_ui' => true,
       'capability_type' => 'post',
+      'taxonomies' => array ('category'),
       'rewrite' => array('slag' => 'service'),
       'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
       )
@@ -92,3 +93,17 @@ function custom_service(){
 }
 
 add_action('init', 'custom_service');
+
+function query_post_type($query){
+  if(is_category() ){
+    $post_type = get_query_var('post_type');
+    if($post_type){
+      $post_type = $post_type;
+    } else {
+      $post_type = array ('post', 'service');
+      $query -> set('post_type', $post_type);
+      return $query;
+    }
+  }
+}
+add_filter('pre_get_posts', 'query_post_type');
